3d04c0985c8ced2bf60b6b09680c5f44e2d648bf
Li Yu <raise.sail@gmail.com>
2012-03-29 23:47:54 +0800

    list.h: copy list_for_each_entry from kernel header

--- list.h
+++ list.h
@@ -137,6 +137,18 @@ static inline void list_del(struct list_head *entry)
 	for (pos = (head)->next, n = pos->next; pos != (head); \
 		pos = n, n = pos->next)
 
+
+/*
+ *  list_for_each_entry  -       iterate over list of given type
+ *  @pos:        the type * to use as a loop cursor.
+ *  @head:       the head for your list.
+ *  @member:     the name of the list_struct within the struct.
+ */
+#define list_for_each_entry(pos, head, member)                          \
+	for (pos = list_entry((head)->next, typeof(*pos), member);      \
+		&pos->member != (head);    \
+		pos = list_entry(pos->member.next, typeof(*pos), member))
+
 /**
  * list_entry - get the struct for this entry
  * @ptr:        the &struct list_head pointer.
