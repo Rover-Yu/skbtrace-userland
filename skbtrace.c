dd79b6d0bc0e0c7a20f7189b674128b66a702b90
Li Yu <raise.sail@gmail.com>
2012-03-31 23:48:06 +0800

    skbtrace.c: fix old email comment

--- skbtrace.c
+++ skbtrace.c
@@ -1,8 +1,6 @@
 /*
  * socket buffer tracing application
  *
- * Copyright (C) 2012 Li Yu <yu.li@alibaba-inc.com>
- *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
@@ -16,6 +14,8 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Copyright (C) 2012 Li Yu <bingtian.ly@taobao.com>
  *
  */
 
