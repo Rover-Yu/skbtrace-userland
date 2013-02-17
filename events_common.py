68fa4438fcf5aab12eb7966d1a56a92fd587f567
Li Yu <bingtian.ly@taobao.com>
2013-02-17 10:09:39 +0800

    rename rps_info to skb_rps_info

--- events_common.py
+++ events_common.py
@@ -3,14 +3,14 @@
 import struct
 import socket
 
-class rps_info:
+class skb_rps_info:
 	action = 1
 	def __init__(self, block, trace):
 		self.blk = block
 		size = block.len - block.common_header_size()
 		data = trace.read(size)
 		if not data:
-			raise ValueError, "invalid rps_info block"
+			raise ValueError, "invalid skb_rps_info block"
 		queue_fmt = "Hxx"
 		rest_fmt = "IIIIIHHI"
 		self.rx_queue = struct.unpack(queue_fmt, data[0:4])
@@ -18,7 +18,7 @@ class rps_info:
 			self.sport, self.dport, self.proto = struct.unpack(rest_fmt, data[4:])
 
 	def __str__(self):
-		s = ["action=rps_info"]
+		s = ["action=skb_rps_info"]
 		s += [" skb=0x%x" % self.blk.ptr]
 		s += [" rx-queue=%d" % self.rx_queue]
 		s += [" rx-hash=0x%x" % self.rx_hash]
@@ -31,5 +31,4 @@ class rps_info:
 		s += [" proto=0x%x" % self.proto]
 		return "".join(s)
 
-events_list = [rps_info]
-
+events_list = [skb_rps_info]
