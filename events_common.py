1dd3440268af5af9b134993562a113c060c58557
Li Yu <bingtian.ly@taobao.com>
2013-02-25 10:03:23 +0800

    skb_delay event support

--- events_common.py
+++ events_common.py
@@ -2,6 +2,7 @@
 
 import struct
 import socket
+from util import *
 
 class skb_rps_info:
 	action = 1
@@ -31,4 +32,46 @@ class skb_rps_info:
 		s += [" proto=0x%x" % self.proto]
 		return "".join(s)
 
-events_list = [skb_rps_info]
+class skb_delay:
+	action = 3
+	def __init__(self, block, trace):
+		self.blk = block
+		size = block.len - block.common_header_size()
+		data = trace.read(size)
+		if not data:
+			raise ValueError, "invalid skb_delay block"
+		fmt = "LLLL"
+		self.sk, self.loc, self.start_sec, self.start_usec = \
+					struct.unpack(fmt, data[:32])
+		data = data[32:]
+
+		slot_fmt = "LL"
+		nr_slots = (size - 32) / 16
+		self.slots = []
+		for slot in range(nr_slots):
+			slot_data = data[slot * 16 : slot * 16 + 16]
+			loc, usec = struct.unpack(slot_fmt, slot_data)
+			self.slots.append((loc, usec))
+
+	def __str__(self):
+		s = ["action=skb_delay"]
+		flags = ""
+		if self.blk.flags & 0x1:
+			flags += "overflow"
+		if self.blk.flags & 0x2:
+			flags += ",error"
+		if flags:
+			s += [" flags=" + flags]
+		s += [" skb=0x%x" % self.blk.ptr]
+		s += [" sock=0x%x" % self.sk]
+		s += [" start_loc=%s" % kallsyms_lookup(self.loc)]
+#		s += [" start_ts=%d.%ds" % (self.start_sec, self.start_usec)]
+		if self.slots:
+			s += [ " history=[" ]
+		for slot in self.slots:
+			s += "(%s,%d) " % (kallsyms_lookup(slot[0]), slot[1])
+		if self.slots:
+			s += ["]"]
+		return "".join(s)
+
+events_list = [skb_rps_info, skb_delay]
